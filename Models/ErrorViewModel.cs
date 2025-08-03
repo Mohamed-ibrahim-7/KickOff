6f4039758dc98fee53d5a5d378e6a679758c9d27
Mohamed-ibrahim-7 <mohamedztz275@gmail.com>
2025-08-03 15:27:00 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KickOff.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
